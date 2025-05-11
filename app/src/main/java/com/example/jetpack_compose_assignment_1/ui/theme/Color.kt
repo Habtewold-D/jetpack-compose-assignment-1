b0833c7127831564503e83de60c62ad9882a71c2
Habtewold-D <habtewold21degfie@gmail.com>
2025-05-11 19:33:05 +0300

    folder structure made

---
+++ app/src/main/java/com/example/jetpack_compose_assignment_1/ui/theme/Color.kt
@@ -0,0 +1,27 @@
+package com.example.jetpack_compose_assignment_1.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary colors
+val PrimaryBlue = Color(0xFF2196F3)
+val PrimaryDarkBlue = Color(0xFF1976D2)
+val PrimaryLightBlue = Color(0xFFBBDEFB)
+
+// Secondary colors
+val SecondaryTeal = Color(0xFF009688)
+val SecondaryDarkTeal = Color(0xFF00796B)
+val SecondaryLightTeal = Color(0xFFB2DFDB)
+
+// Background colors
+val BackgroundLight = Color(0xFFF5F5F5)
+val BackgroundDark = Color(0xFF121212)
+
+// Surface colors
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceDark = Color(0xFF1E1E1E)
+
+// Text colors
+val TextPrimaryLight = Color(0xFF212121)
+val TextSecondaryLight = Color(0xFF757575)
+val TextPrimaryDark = Color(0xFFFFFFFF)
+val TextSecondaryDark = Color(0xFFB3FFFFFF)
