86f5cb0ab22a6554bf5353f24d95949303a59157
Habtewold-D <habtewold21degfie@gmail.com>
2025-05-11 19:38:35 +0300

    course list screen done

---
+++ app/src/main/java/com/example/jetpack_compose_assignment_1/ui/screens/CourseListScreen.kt
@@ -0,0 +1,30 @@
+package com.example.jetpack_compose_assignment_1.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.jetpack_compose_assignment_1.data.Course
+import com.example.jetpack_compose_assignment_1.ui.components.CourseCard
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CourseListScreen(
+    courses: List<Course>,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Academic Courses") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
+        }
+    ) 
+    }
